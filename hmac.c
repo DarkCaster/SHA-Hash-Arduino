e4e141b2d35bdc20cdfdf80759a72808e7bec56f
DarkCaster <dark.caster@outlook.com>
2019-03-07 00:29:54 +0300

    hmac.c updated

--- hmac.c
+++ hmac.c
@@ -1,6 +1,6 @@
 #include "hmac.h"
 
-uint8_t hmac(uint8_t * key, uint8_t keyLen, uint8_t * const opBuff, hashfunc_ptr const hashFunc, const uint8_t hashSize, const uint8_t blockSize)
+uint8_t hmac(uint8_t * key, size_t keyLen, uint8_t * const opBuff, size_t dataLen, hashfunc_ptr const hashFunc, const uint8_t hashSize, const uint8_t blockSize)
 {
 	//TODO: current implementation may not allow hashSize>blockSize
 	//TODO: recheck, may-be hashSize>blockSize situations is not valid at all for HMAC algo
@@ -24,7 +24,9 @@ uint8_t hmac(uint8_t * key, uint8_t keyLen, uint8_t * const opBuff, hashfunc_ptr
 		}
 		memset(key+keyLen,0,blockSize-keyLen);
 	}
-	//TODO: move data to the end of opBuff by blockSize bytes
+	//move data to the end of opBuff by blockSize bytes
+	for(size_t pos=dataLen; pos>0; --pos)
+		opBuff[pos+blockSize-1]=opBuff[pos-1];
 	//create outer padded key and inner padded key
 	for(uint8_t p=0; p<blockSize; ++p)
 	{
@@ -34,6 +36,8 @@ uint8_t hmac(uint8_t * key, uint8_t keyLen, uint8_t * const opBuff, hashfunc_ptr
 		tmpBuff[p]=key[p]^0x5c;
 	}
 	//calculate 1-st pass hash from opBuff that contains concatenated inner padded key and data, and write result to the end of tmpBuff buffer
+	hashFunc(tmpBuff+blockSize, opBuff, dataLen+blockSize);
 	//calculate 2-nd pass hash (final) from tmpBuff buffer that contains concatenated outer padded key and 1-st pass hash, and write result to the start of opBuff
+	hashFunc(opBuff, tmpBuff ,blockSize+hashSize);
 	return hashSize;
 }
--- hmac.h
+++ hmac.h
@@ -9,7 +9,7 @@ extern "C"{
 
 typedef uint8_t (*hashfunc_ptr)(uint8_t * const digest, const uint8_t * const data, size_t len);
 
-uint8_t hmac(uint8_t * key, uint8_t keyLen, uint8_t * opBuff, hashfunc_ptr hashFunc, const uint8_t hashSize, const uint8_t blockSize);
+uint8_t hmac(uint8_t * key, size_t keyLen, uint8_t * const opBuff, size_t dataLen, hashfunc_ptr const hashFunc, const uint8_t hashSize, const uint8_t blockSize);
 
 #ifdef __cplusplus
 } // extern "C"
